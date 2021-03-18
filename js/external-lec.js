49015017d69c2eaeb77e27cf04f2ec8c8b4cf4de
David Wagnon <david.wagnon02@gmail.com>
2021-03-18 15:25:17 -0500

    Went over prompts, consoles, and alerts in js file

--- js/external-lec.js
+++ js/external-lec.js
@@ -1,6 +1,29 @@
 "use strict";
+
 var message = "Howdy y'all";
 
+//Originally was a variable set to a console.log
+//var whatIsThisThen = console.log(message + "Hope y'all are having a mighty fine Thursday");
 var whatIsThisThen = message + "Hope y'all are having a mighty fine Thursday.";
 
 console.log(whatIsThisThen);
+
+// var favoritecheese = prompt(message="What is your favorite cheese?");
+
+// console.log(favoritecheese + " happens to be their favorite cheese. Huzzah!");
+
+// var typeOfPrompt = typeof prompt(message="Tell me something");
+//
+// console.log(typeOfPrompt);
+
+// var num = parseFloat(prompt(message="Gimme a number..."));
+
+// console.log("User entered " + num + ". That number plus 17 is: " + (num += 17) + ".")
+
+// var canIGotToToysRUs = confirm("Can I go to Toys 'R Us, pwease?");
+
+// console.log(canIGotToToysRUs);
+
+// var valueOfThis = alert("You're looking at a website!");
+
+// console.log(valueOfThis);
