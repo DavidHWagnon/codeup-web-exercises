dae8ca1f78d5f50f2d525cb1f6f3894c1a79f1e0
David Wagnon <david.wagnon02@gmail.com>
2021-05-17 10:58:41 -0500

    Completed first 3 exercises

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -36,6 +36,22 @@ const users = [
     }
 ];
 
+// returns an array containing users that know 3 or more languages
+const threeLang = users.filter(n => n.languages.length >2);
+console.log(threeLang);
 
-const evens = users.filter(n => n.languages.length >2);
-console.log(evens);
+//returns an array with all the users emails
+const emails = users.filter(n => n.email);
+console.log(emails);
+
+// returns total years of experience among users
+const total = users.reduce((total, currentNumber) => {
+    return total + currentNumber.yearsOfExperience;
+}, 0);
+console.log(total);
+const average = total / users.length;
+console.log(average);
+
+
+
+
